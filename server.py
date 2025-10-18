9b3ab8d860d01ceae6cc714b7d7693eb65d22067
S B <iris@Ss-MacBook-Pro.local>
2025-10-17 22:12:50 -0700

    Add all files

---
+++ server.py
@@ -0,0 +1,13 @@
+from mcp.server.fastmcp import FastMCP 
+
+mcp = FastMCP('server')
+
+@mcp.tool()
+def greeting(name: str) -> str:
+    "Send a greeting"
+    return f"Hello, {name}!"
+
+
+
+if __name__ == "__main__":
+    mcp.run(transport='streamable-http')
